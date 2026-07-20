3d77095ab97750b27273a66196f58bd8400c3e67
Frog <lyhome2@gmail.com>
2026-07-20 02:15:55 -0700

    Fix Minor Setting UI Bug - Fixes an issue with checkboxes in the settings menu, when checking them there was a slight clipping on the left of the checkbox.

--- CreamInstaller/Forms/SettingsForm.Designer.cs
+++ CreamInstaller/Forms/SettingsForm.Designer.cs
@@ -33,42 +33,45 @@ partial class SettingsForm
         // 
         // darkModeCheckBox
         // 
-        darkModeCheckBox.AutoSize = true;
-        darkModeCheckBox.Location = new Point(20, 20);
-        darkModeCheckBox.Name = "darkModeCheckBox";
-        darkModeCheckBox.Size = new Size(120, 19);
-        darkModeCheckBox.TabIndex = 0;
+        darkModeCheckBox.AutoSize = false;
+            darkModeCheckBox.Location = new Point(36, 20);
+            darkModeCheckBox.Name = "darkModeCheckBox";
+            darkModeCheckBox.Size = new Size(160, 22);
+            darkModeCheckBox.TabIndex = 0;
         darkModeCheckBox.Text = "Enable Dark Mode";
-        darkModeCheckBox.UseVisualStyleBackColor = true;
+        darkModeCheckBox.FlatStyle = FlatStyle.System;
+            darkModeCheckBox.UseVisualStyleBackColor = true;
         settingsToolTip.SetToolTip(darkModeCheckBox, "Switches the application between light and dark color themes. Changes apply immediately.");
         // 
         // blockedGamesCheckBox
         // 
-        blockedGamesCheckBox.AutoSize = true;
-        blockedGamesCheckBox.Location = new Point(20, 50);
-        blockedGamesCheckBox.Name = "blockedGamesCheckBox";
-        blockedGamesCheckBox.Size = new Size(148, 19);
-        blockedGamesCheckBox.TabIndex = 1;
+        blockedGamesCheckBox.AutoSize = false;
+            blockedGamesCheckBox.Location = new Point(36, 50);
+            blockedGamesCheckBox.Name = "blockedGamesCheckBox";
+            blockedGamesCheckBox.Size = new Size(190, 22);
+            blockedGamesCheckBox.TabIndex = 1;
         blockedGamesCheckBox.Text = "Block Protected Games";
-        blockedGamesCheckBox.UseVisualStyleBackColor = true;
+        blockedGamesCheckBox.FlatStyle = FlatStyle.System;
+            blockedGamesCheckBox.UseVisualStyleBackColor = true;
         settingsToolTip.SetToolTip(blockedGamesCheckBox, "Prevents the program from displaying or modifying games protected by anti-cheat software (e.g. Easy Anti-Cheat, BattlEye). Disable at your own risk.");
         // 
         // sortByNameCheckBox
         // 
-        sortByNameCheckBox.AutoSize = true;
-        sortByNameCheckBox.Location = new Point(20, 80);
-        sortByNameCheckBox.Name = "sortByNameCheckBox";
-        sortByNameCheckBox.Size = new Size(160, 19);
-        sortByNameCheckBox.TabIndex = 2;
+        sortByNameCheckBox.AutoSize = false;
+            sortByNameCheckBox.Location = new Point(36, 80);
+            sortByNameCheckBox.Name = "sortByNameCheckBox";
+            sortByNameCheckBox.Size = new Size(200, 22);
+            sortByNameCheckBox.TabIndex = 2;
         sortByNameCheckBox.Text = "Sort game list by name";
-        sortByNameCheckBox.UseVisualStyleBackColor = true;
+        sortByNameCheckBox.FlatStyle = FlatStyle.System;
+            sortByNameCheckBox.UseVisualStyleBackColor = true;
         settingsToolTip.SetToolTip(sortByNameCheckBox, "When enabled, games in the main list are sorted alphabetically by name. When disabled, games appear in their default platform order.");
         // 
         // saveButton
         // 
         saveButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
         saveButton.AutoSize = true;
-        saveButton.Location = new Point(198, 115);
+        saveButton.Location = new Point(220, 115);
         saveButton.Name = "saveButton";
         saveButton.Size = new Size(75, 25);
         saveButton.TabIndex = 3;
@@ -80,7 +83,7 @@ partial class SettingsForm
         // 
         cancelButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
         cancelButton.AutoSize = true;
-        cancelButton.Location = new Point(279, 115);
+        cancelButton.Location = new Point(301, 115);
         cancelButton.Name = "cancelButton";
         cancelButton.Size = new Size(75, 25);
         cancelButton.TabIndex = 4;
@@ -92,7 +95,7 @@ partial class SettingsForm
         // 
         AutoScaleDimensions = new SizeF(7F, 15F);
         AutoScaleMode = AutoScaleMode.Font;
-        ClientSize = new Size(370, 155);
+        ClientSize = new Size(400, 155);
         Controls.Add(cancelButton);
         Controls.Add(saveButton);
         Controls.Add(sortByNameCheckBox);
